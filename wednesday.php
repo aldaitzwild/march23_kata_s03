eee1fdc8a94a9d568b1e05b10bb58682f8614de4
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-04-05 08:50:32 +0200

    Add the kata for wednesday

---
+++ wednesday.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 1 / Créer la fonction getDescSum() qui prend un entier positif en entrée et 
+ *  l'additionne à sa suite dégressif pour renvoyer le total. 
+ * 
+ * Exemple : 
+ * getDescSum(5) 
+ * -> 5 + 4 + 3 + 2 + 1 = 15
+ * return 15
+ * 
+ * getDescSum(3) 
+ * -> 3 + 2 + 1 = 6
+ * return 6
+ * 
+ * getDescSum(8) 
+ * -> 8 + 7 + 6 + 5 + 4 + 3 + 2 + 1 = 36
+ * return 36
+ * 
+ * 
+ * 
+ * 2/ Faire une fonction identique mais cette fois SANS boucle :-)
+ */
