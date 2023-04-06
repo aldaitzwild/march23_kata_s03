38d4ea6b3debce6a27b772527295b6d888fb8d11
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-04-06 09:23:41 +0200

    Add the correction for thursday

--- thursday.php
+++ thursday.php
@@ -30,3 +30,26 @@
  * ...
  * ...
  */
+
+$number = $_GET['number'] ?? 1;
+
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+
+    <ul>
+        <?php for($i = 1; $i < 11; $i++) { ?>
+            <li><?= $number ?> x <?= $i ?> = <a href="thursday.php?number=<?= $number * $i ?>"><?= $number * $i ?></a></li>
+        <?php } ?>
+    </ul>
+    
+ </body>
+ </html>
